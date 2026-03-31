6813081fe62bdcb7fbf3ac9bf9b292622e14eb4a
Indianasol <ideirmendjian@mail.austral.edu.ar>
2026-03-31 10:51:39 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,18 +1,1 @@
-def change():
-    gasto= float(input("ingrese su gasto: "))
-    dinero_recibido = int(input("ingrese el dinero recibido: "))
 
-    vuelto = (dinero_recibido - gasto)
-
-    pesos= round(vuelto)
-    centavos= int((vuelto - pesos)*100)
-    print(f"Ingresar gasto:")
-    print(gasto)
-    print(f"Dinero recibido:")
-    print(dinero_recibido)
-    print(f"\n Vuelto")
-    
-    print(f"Pesos:")
-    print(pesos)
-    print(f"Centavos:")
-    print(centavos)
